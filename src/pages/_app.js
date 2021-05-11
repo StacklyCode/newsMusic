ad073ec850866388f83527a196d279c172c59794
William Jesús Covarrubias Ramos <Skr13@outlook.com>
2021-05-11 14:16:34 -0700

    Feat: Config  Repo

---
+++ src/pages/_app.js
@@ -0,0 +1,11 @@
+import GlobalStyles from "../styles/GlobalStyles";
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />;
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PageHome = () => {
+  return <div>Test React</div>;
+};
+
+export default PageHome;
