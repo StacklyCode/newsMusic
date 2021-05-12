35a82fd33b167fe44d88d5000cc7c082ec2d73a1
Pablo Sabater <pabloosabaterr@gmail.com>
2021-05-12 16:34:11 +0200

    feat: inputAtom

---
+++ src/components/atoms/input/index.js
@@ -0,0 +1,59 @@
+import styled from '@emotion/styled';
+import React, { useEffect } from 'react';
+import useInput from '../../../hooks/useInput';
+
+const Input = styled.div` //toDo change font family to Poppins
+  display: flex;
+  flex-direction: column;
+  font-family: sans-serif;
+  font-weight: 700;
+  p{
+    margin-bottom: 1.25rem;
+    font-size: 1.125rem;
+    color: #A8A8A8;
+  }
+  input{
+    min-height: 4rem;
+    outline: none;
+    border: none;
+    background-color: #1F1F24;
+    color: #c4c4c4;
+    border-radius: 0.625rem;
+    padding: 1rem;
+  }
+`
+
+const InputAtom = ({
+  name,
+  sendValue,
+  lastFormValue,
+  formType,
+  initialValue,
+}) => {
+  const [value, handleInputChange] = useInput({
+    [name]: initialValue[name] || '',
+  });
+
+  let inputValue = value[name];
+
+  useEffect(() => {
+    sendValue({
+      ...lastFormValue,
+      [name]: inputValue,
+    });
+  }, [inputValue]);
+
+  return (
+   <Input>
+      <p>{name}</p>
+      <input
+      onChange={handleInputChange}
+      name={name}
+      value={inputValue}
+      type={formType}
+    />
+   </Input>
+  );
+};
+
+export default InputAtom;
