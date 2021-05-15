63af513aaff5dbef6df4b42ed0a1385667c4e037
karen-marlen <chezmarlenk@gmail.com>
2021-05-14 23:38:41 -0300

    atomButton

---
+++ src/components/atoms/button/index.js
@@ -0,0 +1,39 @@
+import { StyledButton } from './styles';
+
+const AtomButton = ({ 
+    children,
+    color,
+    type,
+    size,
+    href,
+    link
+}) => {
+
+    if (href) {
+        return (
+            <a target="_blank" href={ href }>
+              <StyledButton color={ color } >
+                { children || "Text" }
+              </StyledButton>
+            </a>
+        );
+    }
+
+    if (link) {
+        return (
+          <Link href={ link }>
+            <StyledButton color={ color } outline={ outline }>
+              { children || "Text" }
+            </StyledButton>
+          </Link>
+        );
+    }
+
+    return (
+        <StyledButton type={ type } color={ color } size={ size }>
+          { children || "Text" }
+        </StyledButton>
+      );
+};
+
+export default AtomButton;
---
+++ src/components/atoms/button/style.js
@@ -0,0 +1,57 @@
+import styled from '@emotion/styled';
+
+export const StyledButton = styled.button`
+
+    ${({ color }) => 
+    color === "default" && {
+        backgroundColor: '#2D91DA',
+        border: '1px solid transparent',
+        borderRadius: "10px",
+        color: "#fafafa",
+        cursor: "pointer",
+        fontFamily: "inherit",
+        fontSize: "16",
+        fontWeight: "600",
+        padding: "8px 16px",
+
+        ":hover": {
+            backgroundColor: '#2B8BD0',
+        },
+    }}
+
+    ${({ color }) => 
+    color === "linkers" && {
+        backgroundColor: 'transparent',
+        border: '1px solid transparent',
+        color: "#2D91DA",
+        cursor: "pointer",
+        fontFamily: "inherit",
+        fontSize: "16",
+        fontWeight: "600",
+        letterSpacing: "",
+        padding: "6px 12px",
+        width: "106px",
+        ":hover": {
+            backgroundColor: '',
+        },
+    }}
+
+    ${({ size }) => 
+    size === "medium" && {
+        backgroundColor: '#2D91DA',
+        border: '1px solid transparent',
+        borderRadius: "10px",
+        color: "#fafafa",
+        cursor: "pointer",
+        fontFamily: "inherit",
+        fontSize: "16",
+        fontWeight: "700",
+        padding: "10px 20px",
+        ":hover": {
+            backgroundColor: '#2B8BD0',
+        },
+    }}
+
+    transition: transform 0.4s ease;
+
+`;
