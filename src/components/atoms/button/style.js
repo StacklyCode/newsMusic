3617a3ea0a95b8d29fac07a744f23a7db646c608
karenzhec@gmail.com <karenzhec@gmail.com>
2021-05-20 15:33:25 -0300

    :atomButton

--- src/components/atoms/button/style.js
+++ src/components/atoms/button/style.js
@@ -1,7 +1,6 @@
 import styled from '@emotion/styled';
 
 export const StyledButton = styled.button`
-
     ${({ color }) => 
     color === "default" && {
         backgroundColor: '#2D91DA',
@@ -13,12 +12,10 @@ export const StyledButton = styled.button`
         fontSize: "16",
         fontWeight: "600",
         padding: "8px 16px",
-
         ":hover": {
             backgroundColor: '#2B8BD0',
         },
     }}
-
     ${({ color }) => 
     color === "linkers" && {
         backgroundColor: 'transparent',
@@ -35,7 +32,23 @@ export const StyledButton = styled.button`
             backgroundColor: '',
         },
     }}
-
+    ${({ color }) => 
+    color === "tag" && {
+        backgroundColor: '#1F1F24',
+        border: '1px solid transparent',
+        borderRadius: '7px',
+        color: "#fafafa",
+        cursor: "pointer",
+        fontFamily: "inherit",
+        fontSize: "16",
+        fontWeight: "600",
+        letterSpacing: "",
+        padding: "3px 7px",
+        width: "106px",
+        ":hover": {
+            backgroundColor: '#1B1B20',
+        },
+    }}
     ${({ size }) => 
     size === "medium" && {
         backgroundColor: '#2D91DA',
@@ -51,7 +64,5 @@ export const StyledButton = styled.button`
             backgroundColor: '#2B8BD0',
         },
     }}
-
     transition: transform 0.4s ease;
-
 `;
