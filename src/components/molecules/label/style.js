c45ba7585544a0d49984c2c64be2c51f453fcc81
karen <karenzhec@gmail.com>
2021-05-23 17:09:19 -0300

    Update style.js

--- src/components/molecules/label/style.js
+++ src/components/molecules/label/style.js
@@ -1,9 +1,8 @@
 import styled from '@emotion/styled';
 
 export const StyledLabel = styled.div`
-    border: 1px solid transparent;
-    padding: 4px 5px;
-    width: 90px;
+    border: 1px solid red;
+    padding: 3px 4px;
     justify-content: center;
     display: inline-flex;
     flex-direction: row;
