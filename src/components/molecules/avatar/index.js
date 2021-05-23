a215326d7af77db42cc2257d423a7c66ce741055
karenzhec@gmail.com <karenzhec@gmail.com>
2021-05-23 01:48:06 -0300

    moleculeAvatar

---
+++ src/components/molecules/avatar/index.js
@@ -0,0 +1,29 @@
+import AtomImage from '../../atoms/image';
+import AtomTitle from '../../atoms/title';
+
+import { Avatar } from './style';
+
+const MoleculeAvatar = ({
+    image,
+    title,
+    link
+}) => {
+    return (
+        <Avatar>
+            <AtomImage>
+              <img
+                src={ image } 
+                alt=''
+              />  
+            </AtomImage>
+            <AtomTitle
+                size='subtitleSmall'
+                href={ link }
+            >
+                { title || 'Band Metal' }
+            </AtomTitle>
+        </Avatar>
+    )
+}
+
+export default MoleculeAvatar;
---
+++ src/components/molecules/avatar/style.js
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled';
+
+export const Avatar = styled.div`
+    padding: 5px 10px;
+    align-items: center;
+    border: 1px solid transparent;
+    display: inline-block;
+    font-size: 18px;
+    height: 200px;
+    justify-content: center;
+    text-align: center;
+    width: 180px;
+
+    img {
+        width: 120px;
+        height: 120px;
+        border: 3px solid transparent;
+        border-radius: 120px;
+        cursor: pointer;
+    }
+`;
