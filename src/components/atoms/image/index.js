67313f845f3a4cf28f91979468f1acaeba627fbe
karen <karenzhec@gmail.com>
2021-05-23 01:30:56 -0300

    Update index.js

--- src/components/atoms/image/index.js
+++ src/components/atoms/image/index.js
@@ -1,20 +1,25 @@
 import { Image } from './style';
 
 const AtomImage = ({
-    rounded,
-    width,
-    height,
-    src
+    radius,
+    theme,
+    image
 
 }) => {
     return (
         <Image 
-            rounded={ rounded } 
-            width={ width }
-            height={ height }
+            theme={ theme }
+            radius={ radius }
+            
         >
-            <img 
-                src={ src } alt=''/>
+            <img
+                src={ image || 'https://images.pexels.com/photos/1626481/pexels-photo-1626481.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' }
+                width='100%'
+                height='100%'
+                loading='lazy'
+                alt=''
+
+            />
         </Image>
     );
 }
