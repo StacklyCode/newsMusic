197b8da0b2deea94ae14020762f09d10fec5cc6f
Yuborama <davidjohan2@hotmail.com>
2021-05-29 23:30:18 -0500

    Add: Moleculemap

---
+++ src/components/molecules/map/index.js
@@ -0,0 +1,13 @@
+import { MapStyled } from "./style";
+
+const MoleculeMap = ({ src, width, height }) => {
+  return (
+    <>
+      <MapStyled src={src} width={width} height={height}>
+
+      </MapStyled>
+    </>
+  );
+};
+
+export default MoleculeMap;
